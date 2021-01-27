18d35c6f112f246792109c67078178f03eb35b33
Ruth Wright <rewright26@gmail.com>
2021-01-27 14:44:45 -0700

    Solved exercizes

--- app.js
+++ app.js
@@ -3,5 +3,21 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+// let num = 1;
+// while (num <=100) {
+//     if (num % 2 == 1) {console.log (num)
+//     }
+//     num++;
+// }
+
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+for (var i=1; i < 101; i++) {
+    if (i % 15 == 0) console.log ("FizzBuzz"); 
+    else if (i % 3 == 0) console.log ("Fizz"); 
+    else if (i % 5 == 0) console.log ("Buzz");
+    else console.log (i);
+}
+
